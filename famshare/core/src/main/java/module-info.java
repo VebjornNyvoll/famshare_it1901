6843658ffb31e48703f5ae2ba8b7a5c504be6bae
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-10-04 12:20:55 +0200

    Made changes to pom files and module-info

--- famshare/core/src/main/java/module-info.java
+++ famshare/core/src/main/java/module-info.java
@@ -1,6 +1,6 @@
-module gr2260.famshare.core {
+module famshare.core {
     requires json.simple;
 
-    exports gr2260.famshare.core;
-    exports gr2260.famshare.json;
+    exports famshare.core;
+    exports famshare.json;
 }
