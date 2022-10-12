14cf3e96b62e0bded176d8e98092d988dd1b4f56
Robin Tveråen <robintve@stud.ntnu.no>
2022-10-12 21:05:33 +0200

    added serializer for calendar and updated pom

---
+++ famshare/core/src/main/java/famshare/json/CalendarSerializer.java
@@ -0,0 +1,31 @@
+package famshare.json;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import famshare.core.Calendar;
+
+public class CalendarSerializer extends StdSerializer<Calendar> {
+
+  public CalendarSerializer() {
+    this(null);
+}
+
+  public CalendarSerializer(Class<Calendar> t) {
+    super(t);
+}
+    
+
+  @Override
+  public void serialize(Calendar Calendar, JsonGenerator jsonGen, SerializerProvider serializerProvider)
+   throws IOException {
+    jsonGen.writeStartObject();
+    //Writes the calendar object
+    jsonGen.writeObjectField("calendar", Calendar.getBookings());
+    jsonGen.writeEndObject();
+  }
+
+}
