00480fc200506517c05745344c52157e84fb6da0
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-11-01 13:34:42 +0100

    chore(api): creating restapi module #24
    Made changes to pom.xml files to change project to Java 11 (to support
    spring boot). Also added plugins, dependencies and module to pom.xml
    files. Generated the restapi module using spring initializer
    (https://start.spring.io/) and added the generated files to the module.
    Tests for this module currently do not work, but the project runs as
    usual.
    
    Breaking changes: If test in module is uncommented, project doesn't build.
    
    In this

---
+++ famshare/restapi/src/main/java/gr2260/famshare/famshare/FamshareApplication.java
@@ -0,0 +1,13 @@
+package gr2260.famshare.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamshareApplication.class, args);
+	}
+
+}
---
+++ famshare/restapi/src/test/java/gr2260/famshare/famshare/FamshareApplicationTests.java
@@ -0,0 +1,16 @@
+// package gr2260.famshare.restapi;
+
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class FamshareApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 		assertTrue(true);
+// 	}
+
+// }
