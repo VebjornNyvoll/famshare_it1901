1a19c4849586b4cf8db9f4860576508d50dffae4
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-11-04 12:23:44 +0100

    (chore): Added working example restapi service
    Added example-service from spring boot that works without breaking
    other modules. This allows us to build from this base to make sure we
    know what potentially breaks the service.
    
    This fixes a breaking change introduced in the last commit.

--- famshare/restapi/src/main/java/gr2260/famshare/famshare/FamshareApplication.java
+++ famshare/restapi/src/main/java/gr2260/famshare/famshare/FamshareApplication.java
@@ -1,4 +1,4 @@
-package gr2260.famshare.restapi;
+package gr2260.famshare.famshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FamshareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FamshareApplication.class, args);
-	}
-
-}
+			SpringApplication.run(FamshareApplication.class, args);
+		
+}}
