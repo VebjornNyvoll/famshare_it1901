d592d6bb776d2339594b8b079de8ba192b0d6521
Robin Tveråen <robintve@stud.ntnu.no>
2022-11-14 18:06:15 +0100

    fix: get the correct filepath
    Now uses filepath defined in controller for saving and writing, like it did before

--- famshare/core/src/main/java/famshare/json/Persistance.java
+++ famshare/core/src/main/java/famshare/json/Persistance.java
@@ -2,30 +2,24 @@ package famshare.json;
 
 import java.io.File;
 import java.io.IOException;
-
-import com.fasterxml.jackson.core.exc.StreamWriteException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import famshare.core.Calendar;
 
 public class Persistance {
 
-    private ObjectMapper mapper;
-    private File file;
+    private ObjectMapper mapper = new ObjectMapper();
 
-    public Persistance (File file) {
-        ObjectMapper mapper = new ObjectMapper();
+    public Persistance () {
         mapper.registerModule(new CalendarModule());
-        this.file = file;
     }
 
-    public void writeCalendar (Calendar calendar) throws StreamWriteException, DatabindException, IOException {
-        mapper.writeValue(file, calendar);
+    public void writeCalendar (Calendar calendar, String filepath) throws IOException {
+        mapper.writeValue(new File(filepath), calendar);
     }
 
-    public Calendar readCalendar () throws IOException {
-        return mapper.readValue(file, Calendar.class);
+    public Calendar readCalendar (String filepath) throws IOException {
+        return mapper.readValue(new File(filepath), Calendar.class);
     }
     
 }
