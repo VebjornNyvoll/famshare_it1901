7564fc70901b3b9b0f043f9ea5dc1df3e4b55392
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-11-16 00:29:59 +0100

    fix: Missing file from last commit
    Added HTTPCaller.java file which was missing in the previous commit.

--- famshare/core/src/main/java/famshare/json/HTTPCaller.java
+++ famshare/core/src/main/java/famshare/json/HTTPCaller.java
@@ -1,31 +1,61 @@
 package famshare.json;
 
-import famshare.core.Calendar;
-
-import java.util.List;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
 
 import famshare.core.Booking;
-import kong.unirest.GenericType;
+import famshare.core.Calendar;
+import famshare.core.Item;
+import famshare.core.User;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+
 
 public class HTTPCaller {
     
     public Calendar getCalendarFromAPI(){
-        List<Booking> bookings = Unirest.get("http://localhost:8081/calendar")
-        .asObject(new GenericType<List<Booking>>(){})
-        .getBody();
+        JSONObject rootObj = Unirest.get("http://localhost:8081/calendar")
+        .asJson()
+        .getBody()
+        .getObject();
+
+        // TODO: Move this to CalendarDeserializer
+        JSONArray calendarArray = rootObj.getJSONArray("bookings");
         Calendar calendar = new Calendar();
-        for (Booking booking : bookings) {
+        for (int i = 0; i < calendarArray.length(); i++) {
+            JSONObject bookingObj = calendarArray.getJSONObject(i);
+            Booking booking = new Booking();
+            booking.setBookingId(bookingObj.getInt("bookingId"));
+
+            LocalDate startDate = LocalDate.parse(bookingObj.getString("startDate"));
+            LocalDate endDate = LocalDate.parse(bookingObj.getString("endDate"));
+            booking.setDates(startDate, endDate);
+            
+            JSONObject userObj = bookingObj.getJSONObject("booker");
+
+            User user = new User();
+            user.setName(userObj.getString("name"));
+            user.setId(userObj.getInt("id"));
+            booking.setBooker(user);
+
+            JSONObject itemObj = bookingObj.getJSONObject("bookedObject");
+            Item item = new Item();
+            item.setName(itemObj.getString("name"));
+            item.setId(itemObj.getInt("id"));
+            item.setDescription(itemObj.getString("description"));
+            booking.setBookedObject(item);
+
             calendar.addBooking(booking);
         }
         return calendar;
-        
+
     }
 
-    public void postCalendarToAPI(Calendar calendar){
+    public void postBookingToAPI(Booking booking){
         Unirest.post("http://localhost:8081/calendar")
         .header("Content-Type", "application/json")
-        .body(calendar)
+        .body(booking)
         .asJson();
     }
 
