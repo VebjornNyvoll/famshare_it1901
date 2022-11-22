d5972060851cd5b0549bcd5fbb9f06fd2f2a6ac9
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2022-11-22 12:01:52 +0100

    refactor: added json.simple source code to project
    Due to json.simple being a dependency of the project, its source code
    is now put into a new module named "json_simple". This is because
    json.simple doesn't have a stable module name and therefore we cannot
    package the project without integrating it into the source code.
    
    The restapi does not currently boot with this change.

--- famshare/fxui/src/main/java/module-info.java
+++ famshare/fxui/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module famshare.ui {
     requires famshare.core;
     requires javafx.controls;
     requires javafx.fxml;
-    requires json.simple;
+    requires famshare.json.simple;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires javafx.graphics;
